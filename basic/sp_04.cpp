37b1ffce2af750c6fd6519f02906afb24cb88014
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-08 08:41:50 +0300

    Update sp_04.cpp

--- basic/sp_04.cpp
+++ basic/sp_04.cpp
@@ -1,4 +1,4 @@
-#include "triple.h"
+#include "point.h"
 #include <memory>
 #include <iostream>
 
@@ -6,15 +6,15 @@ int main()
 {
 	using namespace std;
 
-	auto spx = make_shared<Triple>(1, 1, 1);
-	auto spy = make_shared<Triple>(2, 2, 2);
+	auto spx = make_shared<Point>(1, 1, 1);
+	auto spy = make_shared<Point>(2, 2, 2);
 
-	std::cout << "use count for spx : " << spx.use_count() << "\n";
-	std::cout << "use count for spy : " << spy.use_count() << "\n";
+	std::cout << "use count for spx : " << spx.use_count() << '\n';
+	std::cout << "use count for spy : " << spy.use_count() << '\n';
 	spx = spy;
-	std::cout << "bir tusa basin "; std::getchar();
+	std::cout << "press any key"; std::getchar();
 
-	std::cout << "use count for spx : " << spx.use_count() << "\n";
-	std::cout << "use count for spy : " << spy.use_count() << "\n";
-	std::cout << "bir tusa basin "; std::getchar();
+	std::cout << "use count for spx : " << spx.use_count() << '\n';
+	std::cout << "use count for spy : " << spy.use_count() << '\n';
+	std::cout << "press any key "; std::getchar();
 }
