66f06ca4e817ec64b7d47faf18a1d293ff50eec9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-09 07:37:54 +0300

    Update custom_deleter_01.cpp

--- basic/custom_deleter_01.cpp
+++ basic/custom_deleter_01.cpp
@@ -1,18 +1,19 @@
-//deleter constructor'a argüman olarak geçiliyor
+//deleter is passed to the constructor of the class
 
 #include <memory>
-#include "person.h"
-
+#include "point.h"
 
 int main()
 {
+	using namespace std;
+
 	{
-		std::shared_ptr<Person> sptr{ new Person{"Gul", "Eryaman"}, [](Person* p) {
+		shared_ptr<Point> sptr{ new Point{3, 6, 9}, [](Point* p) {
 									std::cout << *p << " is being deleted\n";
 									delete p;
 									} };
 	}
 
-	std::cout << "main is still running\n";
-	//....
+	cout << "main is still running\n";
+	//...
 }
