1f037a8e065c6897db8f124d8dd573f26929ca61
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-09 07:33:25 +0300

    Create deleter_03.cpp

---
+++ basic/deleter_03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct DelNec {
+	void operator()(std::string* p)
+	{
+		std::cout << "DelErg " << *p << '\n';
+		delete p;
+	}
+};
+
+struct DelErg {
+	void operator()(std::string* p)
+	{
+		std::cout << "DelErg " << *p << '\n';
+		delete p;
+	}
+};
+
+int main()
+{
+	using namespace std;
+
+	shared_ptr<string> sp1(new string{"necati"}, DelNec{});
+	shared_ptr<string> sp2(new string{"ergin"}, DelErg{});
+
+	sp1 = sp2;	
+}
