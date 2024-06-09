91933ff94f8377637bbea9107f7f07f8d896444b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-09 07:44:33 +0300

    Update and rename mistake_01.cpp to tm_01.cpp

--- typical_mistakes/tm_01.cpp
+++ typical_mistakes/tm_01.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	using namespace std;
 
-	auto ptr = new string{ "neco" };
+	auto ptr = new string{ "necati ergin" };
 
 	shared_ptr<string> spx{ ptr };
 	shared_ptr<string> spy{ ptr }; //ub
