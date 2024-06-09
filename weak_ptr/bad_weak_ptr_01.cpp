aea090f9a4e4a2e83fcf5b371c0cb8b2f6c1106d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-09 07:50:14 +0300

    Update bad_weak_ptr_01.cpp

--- weak_ptr/bad_weak_ptr_01.cpp
+++ weak_ptr/bad_weak_ptr_01.cpp
@@ -8,20 +8,20 @@ int main()
 	auto sptr = make_shared<Date>(12, 5, 1987);
 	weak_ptr<Date> wp{ sptr };
 
-	cout << "sptr.use_count() = " << sptr.use_count() << "\n";
-	cout << "wp.use_count()   = " << wp.use_count() << "\n";
+	cout << "sptr.use_count() = " << sptr.use_count() << '\n';
+	cout << "wp.use_count()   = " << wp.use_count() << '\n';
 
 	sptr.reset();
 
-	cout << "sptr.use_count() = " << sptr.use_count() << "\n";
-	cout << "wp.use_count()   = " << wp.use_count() << "\n";
+	cout << "sptr.use_count() = " << sptr.use_count() << '\n';
+	cout << "wp.use_count()   = " << wp.use_count() << '\n';
 
 	try {
 		shared_ptr<Date> spx(wp); //throws std::bad_weak_ptr
-		cout << *spx << "\n";
+		cout << *spx << '\n';
 	}
 	//catch (const exception& ex) {
 	catch (const std::bad_weak_ptr& ex) {
-		cout << "hata yakalandi : " << ex.what() << "\n";
+		cout << "hata yakalandi : " << ex.what() << '\n';
 	}
 }
