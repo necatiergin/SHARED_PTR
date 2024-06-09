1478996b9844e4b42d0bd51ac0163ab7c6f2d67d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-09 07:45:24 +0300

    Update sizeof_01.cpp

--- basic/sizeof_01.cpp
+++ basic/sizeof_01.cpp
@@ -8,5 +8,5 @@ int main()
 	shared_ptr<string> sptr{ new string{"necati"} };
 
 	cout << "sizeof(string *) = " << sizeof(string*) << '\n';
-	cout << "sizeof(sptr) = " << sizeof(sptr) << '\n';
+	cout << "sizeof(sptr)     = " << sizeof(sptr) << '\n';
 }
