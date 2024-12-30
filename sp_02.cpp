2bb507f203c6239ac681e8ad0069758aa6ecbdf3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-30 18:09:45 +0300

    Update and rename basic/sizeof_01.cpp to sp_02.cpp

--- sp_02.cpp
+++ sp_02.cpp
@@ -4,8 +4,9 @@
 
 int main()
 {
-	using namespace std;
-	shared_ptr<string> sptr{ new string{"necati"} };
+	using std::cout, std::string;
+
+	std::shared_ptr<string> sptr{ new string{"necati"} };
 
 	cout << "sizeof(string *) = " << sizeof(string*) << '\n';
 	cout << "sizeof(sptr)     = " << sizeof(sptr) << '\n';
