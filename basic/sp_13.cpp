f182a9b7ca6e8eecbb1240a4e0788317141859e1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-30 18:43:44 +0300

    Update and rename in_containers_02.cpp to sp_13.cpp

--- basic/sp_13.cpp
+++ basic/sp_13.cpp
@@ -7,18 +7,16 @@ using Sptr = std::shared_ptr<std::string >;
 
 int main()
 {
-	using namespace std;
-
-	list<Sptr> mylist;
+	std::list<Sptr> mylist;
 	for (int i = 0; i < 10; ++i) {
-		mylist.emplace_back(new string{ rname() });
+		mylist.emplace_back(new std::string{ random_name() });
 	}
 
 	for (auto sptr : mylist)
-		cout << *sptr << " ";
+		std::cout << *sptr << " ";
 	std::cout << "\n\n";
 
-	vector<Sptr> svec;
+	std::vector<Sptr> svec;
 
 	for (auto sp : mylist) {
 		svec.push_back(sp);
@@ -29,7 +27,8 @@ int main()
 		*sp += "can";
 	}
 
+	std::cout << "\n";
 
 	for (auto sptr : mylist)
-		cout << *sptr << " ";
+		std::cout << *sptr << " ";
 }
