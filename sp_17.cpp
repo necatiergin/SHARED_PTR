f3f60b6ae01863fec6222e43b600a3beec930c16
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-06 19:16:52 +0300

    Update sp_17.cpp

--- sp_17.cpp
+++ sp_17.cpp
@@ -1,10 +1,10 @@
 /*
-    shared_ptr aliasing constructor
+	shared_ptr aliasing constructor
 	shared_ptr ile hayatı kontrol edilen bir sınıf nesnesinin veri elemanlarından birini
 	başka bir shared_ptr nesnesinin göstermesini istiyoruz.
 	Eğer bir önlem alınmaz ise sahip olan nesneyi gösteren 	shared_ptr'nin hayatı bitince elemanı gösteren shared_ptr dangling hale gelirdi.
 	Buradaki problemi çözmek için shared_ptr sınıfının "aliasing ctor" denilen ctor'u ile elemana shared_ptr oluşturuyoruz:
-	shared_ptr<Member> spm (spowner, spowner->mx);
+	shared_ptr<Member> spm (spowner, &spowner->mx);
 */
 
 
