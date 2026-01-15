e6567a9e1c98c55b4eb3393aab61f1140962eab8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-01-15 16:41:39 +0300

    Update sp_18.cpp

--- sp_18.cpp
+++ sp_18.cpp
@@ -2,9 +2,6 @@
 #include <vector>
 #include <iostream>
 
-
-using namespace std;
-
 struct Member {};
 
 struct Owner
@@ -22,34 +19,36 @@ using Ovec = std::vector<Owner>;
 
 int main()
 {
-	shared_ptr<Ovec> svecptr(new Ovec); // use_count == 1
+	using std::cout;
+
+	std::shared_ptr<Ovec> svecptr(new Ovec); // use_count == 1
 	svecptr->reserve(10);
 
 	svecptr->emplace_back();
 	svecptr->emplace_back();
 
-	cout << "[1] svecptr.use_count() = " << svecptr.use_count() << "\n";
+	cout << "[1] svecptr.use_count() = " << svecptr.use_count() << '\n';
 
-	shared_ptr<Owner> owner_ptr1(svecptr, &svecptr->at(0));
-	cout << "[2] svecptr.use_count() = " << svecptr.use_count() << "\n";
-	cout << "[3] owner_ptr1.use_count() = " << owner_ptr1.use_count() << "\n";
-	shared_ptr<Owner> owner_ptr2(svecptr, &svecptr->at(1));
-	cout << "[4] svecptr.use_count() = " << svecptr.use_count() << "\n";
-	cout << "[5] optr1.use_count() = " << owner_ptr1.use_count() << "\n";
-	cout << "[6] owner_ptr2.use_count() = " << owner_ptr2.use_count() << "\n";
+	std::shared_ptr<Owner> owner_ptr1(svecptr, &svecptr->at(0));
+	cout << "[2] svecptr.use_count() = " << svecptr.use_count() << '\n';
+	cout << "[3] owner_ptr1.use_count() = " << owner_ptr1.use_count() << '\n';
+	std::shared_ptr<Owner> owner_ptr2(svecptr, &svecptr->at(1));
+	cout << "[4] svecptr.use_count() = " << svecptr.use_count() << '\n';
+	cout << "[5] optr1.use_count() = " << owner_ptr1.use_count() << '\n';
+	cout << "[6] owner_ptr2.use_count() = " << owner_ptr2.use_count() << '\n';
 
-	shared_ptr<Member> member_ptr(svecptr, &svecptr->at(1).mx);
+	std::shared_ptr<Member> member_ptr(svecptr, &svecptr->at(1).mx);
 
-	cout << "[7] svecptr.use_count() = " << svecptr.use_count() << "\n";
-	cout << "[8] optr1.use_count() = " << owner_ptr1.use_count() << "\n";
-	cout << "[9] owner_ptr2.use_count() = " << owner_ptr2.use_count() << "\n";
-	cout << "[10] member_ptr.use_count() = " << member_ptr.use_count() << "\n";
+	cout << "[7] svecptr.use_count() = " << svecptr.use_count() << '\n';
+	cout << "[8] optr1.use_count() = " << owner_ptr1.use_count() << '\n';
+	cout << "[9] owner_ptr2.use_count() = " << owner_ptr2.use_count() << '\n';
+	cout << "[10] member_ptr.use_count() = " << member_ptr.use_count() << '\n';
 
 	svecptr.reset();
 
-	cout << "[11] svecptr.use_count() = " << svecptr.use_count() << "\n";
-	cout << "[12] optr1.use_count() = " << owner_ptr1.use_count() << "\n";
-	cout << "[13] owner_ptr2.use_count() = " << owner_ptr2.use_count() << "\n";
-	cout << "[14] member_ptr.use_count() = " << member_ptr.use_count() << "\n";
+	cout << "[11] svecptr.use_count() = " << svecptr.use_count() << '\n';
+	cout << "[12] optr1.use_count() = " << owner_ptr1.use_count() << '\n';
+	cout << "[13] owner_ptr2.use_count() = " << owner_ptr2.use_count() << '\n';
+	cout << "[14] member_ptr.use_count() = " << member_ptr.use_count() << '\n';
 	(void)getchar();
 }
